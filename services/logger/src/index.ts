cfff96ebdb344e7cdb721aa6f05da4fec4c0dea1
mahpah <ha.pham@digimed.vn>
2017-12-27 10:42:36 +0700

    timestamp

--- services/logger/src/index.ts
+++ services/logger/src/index.ts
@@ -1,12 +1,15 @@
 import { createLogger } from './logger'
 import Config from '@mahpah/configuration'
+import * as moment from 'moment'
 
 const toUpper = (str?: string) => str ? str.toUpperCase() : null
 
 const rabbitConnectionString = Config.rabbitConnectionString || 'amqp://localhost'
 
+const timestamp = () => moment().format('DD/MM/YYYY HH:mm:ss')
+
 createLogger(rabbitConnectionString, {
-  '#': (msg, key) => console.log(`${new Date()} [${toUpper(key) || '*'}] ${msg}`)
+  '#': (msg, key) => console.log(`${timestamp()} [${toUpper(key) || '*'}] ${msg}`)
 })
 .then((queue: string) => {
   console.log(`Successfully connected to ${rabbitConnectionString}`)
